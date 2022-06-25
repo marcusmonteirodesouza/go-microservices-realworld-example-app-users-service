daf759c51211baacb4aef0ec0557247b9aed03f9
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2022-06-25 10:03:22 -0400

    Add build and push container to GCP

---
+++ deploy/gcp/scripts/build-and-push-container-image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PROJECT=$1
+REGION=$2
+IMAGE=$3
+
+DOCKERFILE_PATH='../../..'
+
+pushd "$DOCKERFILE_PATH" || exit 1
+gcloud builds submit --project "$PROJECT" --region "$REGION" --tag "$IMAGE" || exit 1
+popd || exit 1
