868c350aa4f360b67826df58d3709fc9b056bcd7
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2022-07-13 11:46:26 -0400

    Only uses the X-Forwarded-Authorization header when authenticating

--- internal/auth/auth_middleware.go
+++ internal/auth/auth_middleware.go
@@ -27,12 +27,7 @@ func (h AuthMiddleware) Authenticate(next http.HandlerFunc) http.HandlerFunc {
 		const bearerScheme string = "Bearer "
 
 		auth := r.Header.Get("X-Forwarded-Authorization")
-		if len(auth) == 0 {
-			auth = r.Header.Get("Authorization")
-		}
-		if len(auth) == 0 {
-			auth = r.Header.Get("authorization")
-		}
+
 		if !strings.HasPrefix(auth, bearerScheme) {
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 docker compose up -d --build
+go clean -testcache
 JWT_SECRET_KEY=dummy-secret-key JWT_SECONDS_TO_EXPIRE=86400 go test -v ./... && docker compose down
